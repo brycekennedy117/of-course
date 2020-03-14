76b2557d0e6466866b238798d57a4d6cb752f078
Bryce <brycekennedy117@outlook.com>
2020-03-14 19:02:17 +1100

    Added bootstrap to the node dependencies

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.set("views", path.join(__dirname, "templates"));
+app.set("view engine", "pug");
+
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 // create a GET route
 app.get('/', (req, res) => {
-  res.send("Welcome to of-course.");
+  res.render("index", { title: "Home" })
 });
