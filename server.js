f36f8eae5df1e707dd4c4e083491fe30cb1d95b3
Bryce <brycekennedy117@outlook.com>
2020-03-16 19:30:05 +1100

    Integrated favicon and created models folder

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require('express');
 const router = require('./routes');
 const app = express();
 const port = process.env.PORT || 5000;
+const favicon = require('serve-favicon');
 
 app.set("views", path.join(__dirname, "templates"));
 app.set("view engine", "pug");
+
+app.use(favicon(__dirname + '/static/images/favicon.ico'));
 
 app.use(router)
 
